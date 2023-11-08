5f538f1b8cdc98ac65e95716d050e5d11f214b8b
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-08 21:45:52 +0530

    MyJJ8-25

---
+++ MySpring_Boot_aa34vj_JWT_Authentication-Thymeleaf/src/main/java/com/MySpringBootAa26vjJwtAuthenticationThymeleafApplication.java
@@ -0,0 +1,48 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.model.User;
+import com.repo.UserRepository;
+
+@SpringBootApplication
+public class MySpringBootAa26vjJwtAuthenticationThymeleafApplication implements CommandLineRunner {
+
+	public static void main(String[] args){
+		SpringApplication.run(MySpringBootAa26vjJwtAuthenticationThymeleafApplication.class, args);
+	}
+	
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user1 = new User();
+		user1.setId(1);
+		user1.setUsername("Swapnil");
+		user1.setPassword("1234");
+		user1.setRole("ROLE_ADMIN");
+		user1.setEnabled(true);
+		
+		this.userRepo.save(user1);
+		
+		User user2 = new User();
+		user2.setId(2);
+		user2.setUsername("Yash");
+		user2.setPassword("1234");
+		user2.setRole("ROLE_USER");
+		user2.setEnabled(true);
+		
+		this.userRepo.save(user2);
+		
+		
+		
+		
+	}
+
+}
