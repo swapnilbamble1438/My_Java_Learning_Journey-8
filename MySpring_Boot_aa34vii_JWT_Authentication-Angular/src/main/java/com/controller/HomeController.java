2afeef8b0a0aa19a18befde51b4cfe849beea455
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-08 21:42:37 +0530

    MyJJ8-22

---
+++ MySpring_Boot_aa34vii_JWT_Authentication-Angular/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exceptions.ResourceNotFoundException;
+
+@RestController
+@CrossOrigin(origins = "*")      // @CrossOrigin(origins = "*")
+public class HomeController {
+
+	
+	@RequestMapping("/welcome")
+	public String welcome()	
+	{
+			String text = "This is private page";
+			
+			text += "This page is not allowed to unauthenticated users.";
+			
+			return text;
+	}
+	
+	@RequestMapping("/check/{no}")
+	public String checkexception(@PathVariable("no") Integer no)
+	{
+		if(no > 1)
+			throw new ResourceNotFoundException();
+		
+		return "done";
+	}
+	
+	@GetMapping("/getuser")
+	public String getUser()
+	{
+		return "{\"name\":\"swapnil\"}";
+	}
+	
+}
