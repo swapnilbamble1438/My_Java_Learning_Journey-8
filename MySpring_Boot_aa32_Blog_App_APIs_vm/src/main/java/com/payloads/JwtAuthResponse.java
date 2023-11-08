45b7cc8871b0cdcfea37e276b2ed71d1274d3a28
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-08 21:33:26 +0530

    MyJJ8-16

---
+++ MySpring_Boot_aa32_Blog_App_APIs_vm/src/main/java/com/payloads/JwtAuthResponse.java
@@ -0,0 +1,18 @@
+package com.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JwtAuthResponse {
+	
+	private String token;
+
+}
