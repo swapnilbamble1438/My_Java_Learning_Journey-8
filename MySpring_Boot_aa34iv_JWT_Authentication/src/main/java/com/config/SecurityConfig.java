5e04af9266d80a236ebf761623cb99d43e955d4c
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-08 21:39:51 +0530

    MyJJ8-19

---
+++ MySpring_Boot_aa34iv_JWT_Authentication/src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailsService customUserDetailsService;
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		
+		
+		http
+			.csrf()
+			.disable()
+			.cors()
+			.disable()
+			.authorizeHttpRequests()
+			.antMatchers("/token").permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.httpBasic();
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+	
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception
+	{
+		return super.authenticationManagerBean();
+	}
+
+	
+	
+	
+}
