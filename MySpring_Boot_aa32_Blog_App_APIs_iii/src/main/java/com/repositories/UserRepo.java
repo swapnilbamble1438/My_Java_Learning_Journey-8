a77f1227888841dddfc547541c3dfe3026f89fdb
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-08 21:24:01 +0530

    MyJJ8-7

---
+++ MySpring_Boot_aa32_Blog_App_APIs_iii/src/main/java/com/repositories/UserRepo.java
@@ -0,0 +1,13 @@
+package com.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entites.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	
+	
+	
+	
+}
